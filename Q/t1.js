73bc2be6c73d28f045ae7eae90dd0fe607fd2755
surojitpaul <surojit99923@gmail.com>
2024-05-12 20:49:19 +0530

    q and a file swapped

--- Q/t1.js
+++ Q/t1.js
@@ -1,19 +1,24 @@
-// solution for: t1.js
-// 1. 1
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-console.log(array)
-
-// 1. 2
-const evenArray = array.filter(num => num % 2 === 0)
-console.log(evenArray)
-
-// 1. 3
-const evenIndexArray = evenArray.filter((num, index) => index % 2 === 0)
+// 1.1
+// create an array with number 1 to 10
+// and print them in console
 
 
-// 1. 4
-const oddArray = array.filter(num => num % 2 !== 0)
 
-// 1. 5
+// 1.2
+// create another with only even numbers from the first array
+// and print them in console
 
-const joinedArray = [...evenArray, ...oddArray]
+
+
+// 1.3
+// now from the array (1.2) take only elements in even index
+
+
+
+
+// 1.4
+// similar to 1.2 create an array with only odd numbers
+
+
+// 1.5
+// join both array from 1.2 and 1.4 and print them in the console
